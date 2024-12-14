d89acb651f655786b25e304bf36da0dab96325ea
Josue <xJaramis@gmail.com>
2024-12-14 04:28:03 -0800

    Woops

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,22 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const env = loadEnv(mode, process.cwd());
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port:3000,
-    proxy: {
-      "/api": {
-        target: env.VITE_BACKEND_URL,
-        changedOrigin: true
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());  
+
+  // https://vitejs.dev/config/
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port:3000,
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL,
+          changedOrigin: true
+        }
       }
-    }
-  },
-})
+    },
+  })
+}
+
